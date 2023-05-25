94ad334d9fa13bbb6b7fff2982fe2dca8e61e6fa
AlmafuerteAriel <almafuerte.ariel.1978@gmail.com>
2023-05-25 15:48:53 -0300

    Final Push

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import Todo from "./Todo";
 
 const Home = function(props) {
+
+    const { todos } = useSelector((state) => state);
+    console.log(todos);
+
     return (
         <div style={{height: "450px"}} >
-            <h1>Home</h1>
+            <h1>Todo List...</h1>
+            {
+                todos.map((todo) => (
+                    <Todo
+                        text={todo.text}
+                        completed={todo.completed}
+                        id={todo.id}
+                        key={todo.id}
+                    />
+                ))
+            }
         </div>
     )
 };
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { completedTodo, deleteTodo } from "../redux/actions";
+import { Link } from "react-router-dom";
 
-const Todo = function(props) {
+const Todo = function({id, text, completed}) {
+
+    const dispatch = useDispatch();
+    const handleCompleted = (event) => {
+        dispatch(completedTodo(id));
+    }
+    const handleDelete = (event) => {
+        dispatch(deleteTodo(id));
+    }
+
     return (
         <div>
-            <h1>Todo</h1>
+            <Link to={`/detail/${id}`}>
+                <h2
+                    style={completed ? {color: "lightgreen"} : {color: "lightpink"} }
+                >Todo: {text}</h2>
+            </Link>
+            {
+                !completed ? <button onClick={handleCompleted}>Completed</button> : null
+            }
+            {/* Ocultar botón "Delete" si no está completa */}
+            <button onClick={handleDelete}>Delete Todo</button>
         </div>
     )
 };
